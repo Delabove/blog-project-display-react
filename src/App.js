3737d23c8b092ce626f644ea404c95010113a75d
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-19 11:24:10 -0500

    basic navbar structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import backgroundImage from './styling/background.css'
+import backgroundImage from './styling/background.css';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <backgroundImage />
+      <main>
+      <Navbar />
       <header>Hello World</header>
+
+
+      </main>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Navbar(){
+
+    return(
+        <div>
+            <ul>
+                <li>Description</li>
+                <li>Design</li>
+                <li>Technologies</li>
+                <li>Learn</li>
+            </ul>
+        </div>
+    )
+}
