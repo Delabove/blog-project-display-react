fee72d994614a798e76c7a796372e8d35b890f98
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-19 11:12:19 -0500

    project background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import backgroundImage from './styling/background.css'
 
 
 function App() {
   return (
     <div className="App">
+      <backgroundImage />
       <header>Hello World</header>
     </div>
   );
