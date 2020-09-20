83c4d2fa70debf70174fff7437172265647fe119
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-20 08:09:43 -0500

    created router for future component pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import backgroundImage from './styling/background.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Layout from './components/Layout';
+import Main from './components/Main';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <backgroundImage />
 
         <Navbar />
-
+        <Main />
         <Layout />
 
 
