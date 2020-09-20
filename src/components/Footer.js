e6c4b9854520bbcfb768867a5cb7a161147dc4e8
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-20 07:25:39 -0500

    added social icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import footer from '../styling/footer.css';
+import insta from '../media/Insta.png';
+import linkedin from '../media/linkedin.png';
+import portal from '../media/alumniPortal.png';
+import github from '../media/github.png';
 
 
 export default function Navbar(){
     return(
         <footer>
             <ul className="footer-container">
-                <a href="#"><li className="footer-item">LinkedIn</li></a>
-                <a href="#"><li className="footer-item">GitHub</li></a>
-                <a href="#"><li className="footer-item">Portal</li></a>
-                <a href="#"><li className="footer-item">Insta</li></a>
+                <div class="social"><a  href="https://www.linkedin.com/in/delayne-labove/"><li className="footer-item"><img src={linkedin} width="100%" alt="social" /></li></a></div>
+                <div class="social" ><a href="https://github.com/Delabove"><li className="footer-item"> <img src={github} width="100%" alt="social" /></li></a></div>
+                <div class="social" ><a href="https://alumniPortal.com/Delabove"><li className="footer-item"> <img src={portal} width="100%" alt="social" /></li></a></div>
+                <div  class="social" ><a href="https://www.instagram.com/color2code/"><li className="footer-item"> <img src={insta} width="100%" alt="social" /></li></a></div>
             </ul>
         </footer>
     )
