73ab305fa44c1f7cb53686824f04f57804ae8ec2
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-20 13:10:39 -0500

    global layout  child rendering to parent component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import backgroundImage from './styling/background.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Layout from './components/Layout';
 import Main from './components/Main';
-// import TexasFresh from './components/TexasFresh';
+import TexasFresh from './components/TexasFresh';
+import SocialCode from './components/SocialCode';
 
 
 function App() {
   return (
     <div className="App">
       <backgroundImage />
+      <BrowserRouter>
+      <div>
+        <header>
+          <Navbar/>
+        </header>
+        <main>
 
-        <Navbar />
-        <Main />
-        <Layout />
-        {/* <TexasFresh /> */}
-
-
-        <Footer />
-
-
+          <React.Suspense fallback="Loading...">
+            <Switch>
+              <Route path="/texasFresh">
+                <TexasFresh/>
+              </Route>
+              <Route path="/socialCode">
+              <SocialCode />
+              </Route>
+            </Switch>
+          </React.Suspense>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+      </BrowserRouter>
 
     </div>
   );
 }
 
 export default App;
+
+
+// <Navbar />
+// <Main />
+// <Layout>
+//   <TexasFresh />
+// </Layout>
+
+
+
+// <Footer />
+
+
+
+  // {/* <div className="layout" xs={12} md={6}>
+  //               <ul className="layout-list">
+  //                   <li className="description">
+  //                   <h2>Description </h2>
+  //                       <div>{desc}</div>
+  //                   </li>
+  //                   <li className="design">
+  //                   <h1>Design</h1>
+  //                   <p>{des}</p>
+  //                   <p>{img}</p>
+  //                   <p>{des2}</p>
+  //                   </li>
+  //                   <li className="technologies">
+  //                   <h2>Technologies</h2>
+  //                   <p>{tech}</p>
+  //                   </li>
+  //                   <li className="learns">
+  //                   <h2>Learns</h2>
+  //                   <div> {learn}</div>
+  //                   </li>
+  //               </ul>
+  //           </div> */}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,26 +6,7 @@ export default function Layout(props){
     return(
         // <Link to="/layout">
             <div className="layout" xs={12} md={6}>
-                <ul className="layout-list">
-                    <li className="description">
-                    <h2>Description </h2>
-                        <div>{props.description}</div>
-                    </li>
-                    <li className="design">
-                    <h1>Design</h1>
-                    <p>{props.design}</p>
-                    <p>{props.img}</p>
-                    <p>{props.design2}</p>
-                    </li>
-                    <li className="technologies">
-                    <h2>Technologies</h2>
-                    <p>{props.tech}</p>
-                    </li>
-                    <li className="learns">
-                    <h2>Learns</h2>
-                    <div> {props.learns}</div>
-                    </li>
-                </ul>
+                {props.children}
             </div>
         // </Link>
     )
