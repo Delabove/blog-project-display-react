4b4e13449742dff148fbdfb869d54d1d21d3e2d5
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-20 22:43:23 -0500

    texas fresh complete and deployment

--- src/components/TexasFresh.js
+++ src/components/TexasFresh.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Style from '../styling/layout.css';
 import Layout from './Layout';
+import Image from '../media/texasFresh.png';
+import desc from '../media/description.png';
+import design from '../media/design.png';
+import technologies from '../media/technologies.png';
+import learns from '../media/learns.png';
 
 export default function TexasFresh(){
     return(
         <Link to="/texas-Fresh">
         {/* <Layout> */}
         <div className="layout" xs={12} md={6}>
-                <ul className="layout-list">
-                    <li className="description">Description
-                        <div> This is different!!! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos velit illo necessitatibus accusamus rerum aperiam quaerat, animi perferendis iure maxime esse a similique nam aliquid tenetur dignissimos consequatur repudiandae consectetur facere laudantium. </div>
+        <ul className="layout-list">
+                    <li className="description">
+                    <p><img width="100%" className="img" src={desc} alt="social" /></p>
+                        <div className="project-body">Texas Fresh is a blog landing page for a ficticious local farmer. The landing page contains an eyebrow nav, navbar with 5 blog topics, 5 featured posts, an archive section, a newletter signup form, and copyright in the footer.</div>
                     </li>
                     <li className="design">
-                    <h1>Design</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam consequatur quam explicabo culpa nam minima. Temporibus praesentium, reprehenderit, cupiditate odit, autem minima eius vel dicta ab a veritatis. Modi, reprehenderit!</p>
-                    <h3>This is an image</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi iste voluptatum excepturi, id consequuntur consequatur nemo quia dignissimos iusto doloribus laborum rem, aperiam molestiae quisquam illo sunt commodi fugit sequi?</p>
+                    <p><img width="110%" className="smaller-title" src={design} alt="social" /></p>
+                    <p className="project-body"> Texas Fresh blog was my first full frontend project outside of school that was assigned to me for an interview assessment!</p>
+                    <p><img width="100%" className="image" src={Image} alt="social" /></p>
+                    <p className="project-body">I thoroughly enjoyed building this project. Prior to beginning, I had spent a little time learning React. I thought it would be a great project to take a deeper dive into the JavaScript library! I had no idea what I would learn during this project, but, to date, it is my favorite project. I preplanned the project using InVision. You can checkout the wireframe for Texas Fresh <a className="wireframe" href="https://projects.invisionapp.com/freehand/document/aOcwTFuN7">here</a>. I, also, created all graphics using canva pro.</p>
                     </li>
-                    <li className="technologies">Technologies
-                    <p>This is a list of technologies to make it look like there is a list of technologies in div.</p>
+                    <li className="technologies">
+                    <p><img width="100%" className="img" src={technologies} alt="social" /></p>
+                    <p className="project-body">HTML, React and Material UI</p>
                     </li>
                     <li className="learns">
-                    <div> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor at maiores, quibusdam quia facilis vitae culpa distinctio officia consequuntur, expedita deleniti facere ex veniam, voluptatibus voluptatum non? Rerum delectus perspiciatis, illum temporibus atque laudantium odio aspernatur reiciendis ad libero sequi fuga voluptatibus neque odit? Assumenda eveniet tempore modi esse nemo, id at impedit quos libero, animi quam nulla minus! Dolores dolorem dolorum corporis natus veritatis! Nobis sunt, dolorum ipsa odit possimus eum itaque ex. Corporis ipsa sed fuga nihil! Perspiciatis corrupti a illum nostrum pariatur, harum ratione odio totam doloribus necessitatibus, aliquid culpa laudantium aut nemo sapiente vero aperiam officiis!</div>
+                    <p><img width="100%" className="smaller-title" src={learns} alt="social" /></p>
+                    <div className="project-body"> One of my favorite learns from Texas Fresh was fully experiencing the benefits of breaking down each project into components. This is a common practice with React and I can't see me structuring future personal projects any other way! The biggest "Aha!" occurred when I was creating individual objects and rendering the data into the parent component by mapping through the object data and using props to feed the fields into their respective elements. I learned so much with this project and it has helped me see how far I have come as a new developer!</div>
                     </li>
                 </ul>
             </div>
